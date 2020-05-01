5ee54c1d0f0212be04ae2935ad674a77163a1d49
songkyunghee <kyunghee794613@gmail.com>
2020-05-01 17:59:52 +0900

    first commit

---
+++ app/src/main/java/com/kyunghee/test_mathpresso/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kyunghee.test_mathpresso
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
